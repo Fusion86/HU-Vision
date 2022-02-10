4d5af6f9329265ba4ed2b20d6aa8b0a9b8d5ab50
Fusion86 <fusion86@cerbus.nl>
2022-02-10 20:54:24 +0100

    Maybe finish Convoluties

--- assignments/Convoluties/src/main.py
+++ assignments/Convoluties/src/main.py
@@ -1,44 +1,34 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from skimage import data, filters, feature
+from skimage import data, filters, feature, util
 from scipy import ndimage
 
+
+def make_filter(array_like):
+    return np.array(array_like)
+
+
 if __name__ == "__main__":
-    image = data.camera()
+    image = util.img_as_float(data.camera())
 
     # Blur image
-    blur_filter = [[1 / 9, 1 / 9, 1 / 9]] * 3
+    # blur_filter = [[1 / 9, 1 / 9, 1 / 9]] * 3
+    blur_filter = make_filter([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
     blur_image = ndimage.convolve(image, blur_filter)
     blur_image = ndimage.convolve(blur_image, blur_filter)
 
-    # Positive Laplacian Operator
-    laplacian_filter = [
-        [0, 1, 0],
-        [1, -4, 1],
-        [0, 1, 0],
-    ]
-
+    # Negative Laplacian Operator
+    laplacian_filter = make_filter([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
     laplacian_image = ndimage.convolve(blur_image, laplacian_filter)
 
     # Sobel Horizontal
-    sobel_h_filter = [
-        [-1, 0, 1],
-        [-2, 0, 2],
-        [-1, 0, 1],
-    ]
-    sobel_v_filter = [
-        [1, 2, 1],
-        [0, 0, 0],
-        [-1, -2, -1],
-    ]
+    sobel_h_filter = make_filter([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    sobel_v_filter = make_filter([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
     sobel_h_image = ndimage.convolve(blur_image, sobel_h_filter)
     sobel_v_image = ndimage.convolve(blur_image, sobel_v_filter)
 
     # Scharr
-    scharr_h_filter = [
-        [47, 0, -47],
-        [162, 0, -162],
-        [47, 0, -47],
-    ]
+    scharr_h_filter = make_filter([[47, 0, -47], [162, 0, -162], [47, 0, -47]])
     scharr_h_image = ndimage.convolve(blur_image, scharr_h_filter)
 
     # Using skimage filters
@@ -47,10 +37,10 @@ if __name__ == "__main__":
     sk_scharr = filters.scharr(image)
 
     # Canny filter
-    canny_default_image = feature.canny(image)
+    canny_default_image = feature.canny(data.camera())
 
     canny_image = feature.canny(
-        image, sigma=1.4, low_threshold=0.4 * 255, high_threshold=0.5 * 255
+        data.camera(), sigma=1.4, low_threshold=0.4 * 255, high_threshold=0.5 * 255
     )
 
     # Show results
@@ -59,7 +49,7 @@ if __name__ == "__main__":
     )
 
     ax1.imshow(laplacian_image, cmap="gray")
-    ax1.set_title("Positive Laplacian Operator")
+    ax1.set_title("Negative Laplacian Operator")
 
     ax2.imshow(sobel_h_image, cmap="gray")
     ax2.set_title("Sobel Horizontal Operator")
@@ -81,5 +71,8 @@ if __name__ == "__main__":
 
     ax8.imshow(canny_image, cmap="gray")
     ax8.set_title("Canny (Tweaked Params)")
+
+    ax9.imshow(blur_image, cmap="gray")
+    ax9.set_title("Blurred Image")
 
     plt.show()
